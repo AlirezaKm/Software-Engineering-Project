948aa2dcc91fb5d08b4fa03f6ad150f85b8b97ad
Alireza Km <alitm28@gmail.com>
2017-07-01 10:37:11 +0430

    change config/database.php , add oauth private , public keys

--- API/config/database.php
+++ API/config/database.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'pgsql'), //pgsql
+    'default' => env('DB_CONNECTION', 'mysql'), //pgsql
 
     /*
     |--------------------------------------------------------------------------
